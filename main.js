7ad7712bb2dca69d14b165225a8c18816e5b85ba
brenden <brenden@somethinglikecorkscrews.com>
2015-03-07 19:48:01 -0600

    hopefully this works

---
+++ main.js
@@ -0,0 +1,255 @@
+//necessary global variables
+var camera;
+var controls;
+var scene;
+var renderer;
+var stats;
+var sim;
+var cloudUniforms;
+var cloudMaterial;
+var cloud;
+var velocityField;
+var velocitySim;
+
+var previousTime;
+
+var mouse = new THREE.Vector3();
+document.onmousemove = function(event) {
+    mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
+    mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
+    mouse.z = 0.5;
+};
+var mouseIntersect = new THREE.Vector3(0,-1,0);
+var intersectPlane = new THREE.Plane(new THREE.Vector3(0,1,0));
+
+var textureLength = 512;
+var particleCount = textureLength * textureLength;
+var gridSize = 320;
+var spaceBetweenGridCells = 5;
+var velocityMapWidth = gridSize / spaceBetweenGridCells ;
+
+var simulate = false;
+
+
+
+//when the user hits the spacebar stop/start the simulation
+document.onkeypress = function(e){
+    if(e.charCode === 32){
+        if(simulate){
+            simulate = false;
+        }
+        else{
+            simulate = true;
+        }
+    }
+    else if (e.charCode === 101){
+        var tempMouse = new THREE.Vector3(mouse.x,mouse.y,mouse.z);
+        tempMouse.unproject(camera);
+        console.log(tempMouse);
+        var ray = new THREE.Ray( camera.position, tempMouse.sub( camera.position ).normalize() );
+        console.log(ray);
+        var sect = ray.intersectPlane(intersectPlane);
+        console.log(sect);
+        if(sect !== null){
+            mouseIntersect = sect
+        }
+        console.log(mouseIntersect);
+    }
+}
+
+var initialize = function(){
+    // renderer
+    renderer = new THREE.WebGLRenderer( { antialias: false } );
+    renderer.setPixelRatio( window.devicePixelRatio );
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    document.body.appendChild(renderer.domElement);
+
+    //velocity texture map for simulation
+    velocityField = new VelocityTextureMap({
+        width : velocityMapWidth,
+        renderer : renderer
+    });
+
+    //simulator
+    sim = new ParticleSimulator({
+        width : textureLength,
+        bounds : gridSize,
+        renderer : renderer,
+        particleShaderId : "particleFragment",
+        uniforms: {
+            delta: { type: "f", value: 0.0 },
+            bounds : {type : "f", value : gridSize / 2},
+            resolution: { type: "v2", value: new THREE.Vector2( textureLength, textureLength ) },
+            texture: { type: "t", value: null },
+            particleVelocities : { type : "t", value : null}
+        }
+    });
+    sim.initialize();
+
+    velocitySim = new VelocitySimulator({
+        width : textureLength,
+        bounds : gridSize,
+        renderer:renderer,
+        particleShaderId : "velocityFragment",
+        uniforms: {
+            delta: { type: "f", value: 0.0 },
+            bounds : {type : "f", value : gridSize / 2},
+            mouse : {type : "v3", value :mouseIntersect},
+            resolution: { type: "v2", value: new THREE.Vector2( textureLength, textureLength ) },
+            texture: { type: "t", value: null },
+            positionField : {type : "t", value : null},
+            velocityField : { type : "t", value : velocityField.texture}
+        }
+    });
+    velocitySim.initialize();
+    velocitySim.renderTexture({
+        delta : 0.5,
+        positionField : sim.activeTexture
+    });
+
+    //so it looks nice at initialization
+    sim.renderTexture({
+        delta : 0.5,
+        particleVelocities : velocitySim.activeTexture
+    });
+
+
+
+    //camera
+    camera = new THREE.PerspectiveCamera(60,window.innerWidth / window.innerHeight,1,1000);
+    camera.position.z = 100;
+
+    camera.position.y = 100;
+    //controls
+    controls = new THREE.OrbitControls( camera );
+    controls.damping = 0.2;
+
+    //scene
+    scene = new THREE.Scene();
+
+    //stats
+    stats = new Stats();
+    stats.domElement.style.position = 'absolute';
+    stats.domElement.style.top = '0px';
+    stats.domElement.style.zIndex = 100;
+    document.body.appendChild( stats.domElement );
+    window.addEventListener( 'resize', onWindowResize, false );
+
+    //GridHelper
+    //scene.add(new THREE.GridHelper(gridSize / 2, spaceBetweenGridCells));
+
+    var cloudAttributes = {
+        size:        { type: 'f', value: null },
+        customColor: { type: 'c', value: null },
+        reference :  { type: 'v2', value: null}
+    };
+
+    cloudUniforms = {
+        color:     { type: "c", value: new THREE.Color( 0xffffff ) },
+        texture:   { type: "t", value: null }
+    };
+
+    cloudMaterial = new THREE.ShaderMaterial( {
+        uniforms:       cloudUniforms,
+        attributes:     cloudAttributes,
+        vertexShader:   document.getElementById( 'cloudVertex' ).textContent,
+        fragmentShader: document.getElementById( 'cloudFragment' ).textContent,
+        blending:       THREE.AdditiveBlending,
+        depthTest:      false,
+        transparent:    true
+    });
+
+    var makeParticles = function(){
+        //make a particle system with buffer geometry
+        var geometry = new THREE.BufferGeometry();
+
+        var positions = new Float32Array( particleCount * 3 );
+        var values_color = new Float32Array( particleCount * 3 );
+        var values_size = new Float32Array( particleCount );
+        var references = new Float32Array(particleCount * 2);
+
+        var color = new THREE.Color();
+
+        for( var v = 0; v < particleCount; v++ ) {
+
+            //wow this is dumb, this is how glsl indexes texture positions, as a fraction of the overall length
+            //instead of something sane like an integer based index
+            references[v * 2] = (Math.floor(v / textureLength)) / textureLength;
+            references[(v * 2) + 1] = (v % textureLength) / textureLength;
+            //nevermind that's actually pretty smart?
+
+            //whatever, copypasta'ed code
+            values_size[ v ] = 0.3;
+
+            //positions don't matter since we'll be taking those from the texture anyways
+            positions[ v * 3 ] = Math.random() * gridSize - gridSize / 2;
+            positions[ v * 3 + 1 ] = Math.random() * gridSize - gridSize / 2;
+            positions[ v * 3 + 2 ] = Math.random() * gridSize - gridSize / 2;
+
+            //color, whatever
+            color.setHSL( v / particleCount, 1.0, 0.5 );
+            values_color[ v * 3 ] = color.r;
+            values_color[ v * 3 + 1 ] = color.g;
+            values_color[ v * 3 + 2 ] = color.b;
+
+        }
+
+        geometry.addAttribute( 'position', new THREE.BufferAttribute( positions, 3 ) );
+        geometry.addAttribute( 'customColor', new THREE.BufferAttribute( values_color, 3 ) );
+        geometry.addAttribute( 'size', new THREE.BufferAttribute( values_size, 1 ) );
+        geometry.addAttribute( 'reference', new THREE.BufferAttribute( references, 2 ) );
+        geometry.computeBoundingBox();
+
+        cloud = new THREE.PointCloud( geometry, cloudMaterial );
+
+        scene.add( cloud );
+    }
+
+    makeParticles();
+
+
+    previousTime = window.performance.now();
+    render();
+    animate();
+}
+
+function onWindowResize() {
+
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+
+    renderer.setSize( window.innerWidth, window.innerHeight );
+
+    render();
+}
+
+
+function render() {
+    renderer.render( scene, camera );
+    stats.update();
+}
+
+function animate() {
+    var now = window.performance.now();
+    var delta = previousTime - now;
+    var previousTime = now;
+
+    if(simulate){
+        velocitySim.renderTexture({
+            mouse : mouseIntersect,
+            delta : delta,
+            positionField : sim.activeTexture
+        });
+        sim.renderTexture({
+            delta : delta,
+            particleVelocities : velocitySim.activeTexture
+        });
+        cloudUniforms.texture.value = sim.activeTexture;
+        mouseIntersect.y = -1;
+    }
+    renderer.render(scene,camera);
+    stats.update();
+    controls.update();
+
+    requestAnimationFrame(animate);
+}
---
+++ texture/velocityTexture.js
@@ -0,0 +1,50 @@
+/**
+ * Created by brenden on 3/2/15.
+ */
+var VelocityTextureMap = function(options) {
+    var self = this instanceof VelocityTextureMap ? this : Object.create(VelocityTextureMap.prototype);
+    ShaderTexture.call(self,options);
+
+    self.width = options.width;
+
+    //all we're doing here is creating a static texture (not a RenderTarget)
+    //and using it for updating the paticles positions
+    (function(){
+        //make the typed array for the texture
+        var textureArray = new Float32Array( self.width * self.width * 4 );
+
+        var negative = function(){
+            if(Math.random() < 0.5) return -1;
+            else return 1;
+        }
+
+        //loop through the data
+        for ( var k = 0; k < textureArray.length; k += 4 ) {
+            var x = Math.random() * 1 * negative();
+            //2d for now
+            var y = 0;
+            var z = Math.random() * 1 * negative();
+            //rgba
+            textureArray[ k + 0 ] = x;
+            textureArray[ k + 1 ] = y;
+            textureArray[ k + 2 ] = z;
+            textureArray[ k + 3 ] = 1;
+        }
+
+        texture = new THREE.DataTexture( textureArray, self.width, self.width, THREE.RGBAFormat, THREE.FloatType );
+        texture.minFilter = THREE.NearestFilter;
+        texture.magFilter = THREE.NearestFilter;
+        texture.needsUpdate = true;
+        texture.flipY = false;
+
+        var pingTexture = self.getRenderTarget(THREE.RGBAFormat,self.width);
+        self.initializeTexture(texture,pingTexture);
+
+        self.texture = pingTexture;
+
+    })();
+
+    return self;
+};
+
+VelocityTextureMap.prototype = Object.create(ShaderTexture.prototype);
